bfd4f8c7d0336f2522a5ad6397b507103e3a5549
Honed <tarreinthway@gmail.com>
2026-05-28 23:32:31 +0630

    Update app.py

--- app/app.py
+++ app/app.py
@@ -27,16 +27,17 @@ BLOB_NAME = "food101_densenet201_finetune.keras"
 MODEL_PATH = "/tmp/food101_densenet201_finetune.keras"
 
 def load_model_from_azure():
-    if not os.path.exists(MODEL_PATH):
-        account_url = f"https://fooddddstorage.blob.core.windows.net"
-        credential = DefaultAzureCredential()
-        blob_service_client = BlobServiceClient(account_url, credential=credential)
-        blob_client = blob_service_client.get_blob_client(container="model-weights", blob="food101_densenet201_finetune.keras")
-        with open(MODEL_PATH, "wb") as download_file:
-            download_file.write(blob_client.download_blob().readall())
-    return tf.keras.models.load_model(MODEL_PATH)
-
-model = load_model_from_azure()
+    global model
+    if model is None:
+        if not os.path.exists(MODEL_PATH):
+            account_url = f"https://fooddddstorage.blob.core.windows.net"
+            credential = DefaultAzureCredential()
+            blob_service_client = BlobServiceClient(account_url, credential=credential)
+            blob_client = blob_service_client.get_blob_client(container="model-weights", blob="food101_densenet201_finetune.keras")
+            with open(MODEL_PATH, "wb") as download_file:
+                download_file.write(blob_client.download_blob().readall())
+        model = tf.keras.models.load_model(MODEL_PATH)
+    return model
 
 #Class names
 class_names = ['apple_pie', 'baby_back_ribs', 'baklava', 'beef_carpaccio', 'beef_tartare', 'beet_salad', 'beignets', 'bibimbap', 'bread_pudding', 'breakfast_burrito', 'bruschetta', 'caesar_salad', 'cannoli', 'caprese_salad', 'carrot_cake', 'ceviche', 'cheesecake', 'cheese_plate', 'chicken_curry', 'chicken_quesadilla', 'chicken_wings', 'chocolate_cake', 'chocolate_mousse', 'churros', 'clam_chowder', 'club_sandwich', 'crab_cakes', 'creme_brulee', 'croque_madame', 'cup_cakes', 'deviled_eggs', 'donuts', 'dumplings', 'edamame', 'eggs_benedict', 'escargots', 'falafel', 'filet_mignon', 'fish_and_chips', 'foie_gras', 'french_fries', 'french_onion_soup', 'french_toast', 'fried_calamari', 'fried_rice', 'frozen_yogurt', 'garlic_bread', 'gnocchi', 'greek_salad', 'grilled_cheese_sandwich', 'grilled_salmon', 'guacamole', 'gyoza', 'hamburger', 'hot_and_sour_soup', 'hot_dog', 'huevos_rancheros', 'hummus', 'ice_cream', 'lasagna', 'lobster_bisque', 'lobster_roll_sandwich', 'macaroni_and_cheese', 'macarons', 'miso_soup', 'mussels', 'nachos', 'omelette', 'onion_rings', 'oysters', 'pad_thai', 'paella', 'pancakes', 'panna_cotta', 'peking_duck', 'pho', 'pizza', 'pork_chop', 'poutine', 'prime_rib', 'pulled_pork_sandwich', 'ramen', 'ravioli', 'red_velvet_cake', 'risotto', 'samosa', 'sashimi', 'scallops', 'seaweed_salad', 'shrimp_and_grits', 'spaghetti_bolognese', 'spaghetti_carbonara', 'spring_rolls', 'steak', 'strawberry_shortcake', 'sushi', 'tacos', 'takoyaki', 'tiramisu', 'tuna_tartare', 'waffles']
