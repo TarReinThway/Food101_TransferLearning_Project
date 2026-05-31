095fea3d3f886d3c6fdc272362015520b1d4492e
TarReinThway <tarreinthway@gmail.com>
2026-05-31 11:48:59 +0630

    Some changes in app.py

--- app/app.py
+++ app/app.py
@@ -30,8 +30,16 @@ UPLOAD_FOLDER = os.path.join(BASE_DIR, "static", "uploads")
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-ort_session = ort.InferenceSession(MODEL_PATH)
-input_name = ort_session.get_inputs()[0].name
+ort_session = None
+input_name = None
+
+def get_onnx_session():
+    global ort_session, input_name
+    if ort_session is None:
+        print("Initializing ONNX model session...")
+        ort_session = ort.InferenceSession(MODEL_PATH)
+        input_name = ort_session.get_inputs()[0].name
+    return ort_session, input_name
 
 #Class names
 class_names = ['apple_pie', 'baby_back_ribs', 'baklava', 'beef_carpaccio', 'beef_tartare', 'beet_salad', 'beignets', 'bibimbap', 'bread_pudding', 'breakfast_burrito', 'bruschetta', 'caesar_salad', 'cannoli', 'caprese_salad', 'carrot_cake', 'ceviche', 'cheesecake', 'cheese_plate', 'chicken_curry', 'chicken_quesadilla', 'chicken_wings', 'chocolate_cake', 'chocolate_mousse', 'churros', 'clam_chowder', 'club_sandwich', 'crab_cakes', 'creme_brulee', 'croque_madame', 'cup_cakes', 'deviled_eggs', 'donuts', 'dumplings', 'edamame', 'eggs_benedict', 'escargots', 'falafel', 'filet_mignon', 'fish_and_chips', 'foie_gras', 'french_fries', 'french_onion_soup', 'french_toast', 'fried_calamari', 'fried_rice', 'frozen_yogurt', 'garlic_bread', 'gnocchi', 'greek_salad', 'grilled_cheese_sandwich', 'grilled_salmon', 'guacamole', 'gyoza', 'hamburger', 'hot_and_sour_soup', 'hot_dog', 'huevos_rancheros', 'hummus', 'ice_cream', 'lasagna', 'lobster_bisque', 'lobster_roll_sandwich', 'macaroni_and_cheese', 'macarons', 'miso_soup', 'mussels', 'nachos', 'omelette', 'onion_rings', 'oysters', 'pad_thai', 'paella', 'pancakes', 'panna_cotta', 'peking_duck', 'pho', 'pizza', 'pork_chop', 'poutine', 'prime_rib', 'pulled_pork_sandwich', 'ramen', 'ravioli', 'red_velvet_cake', 'risotto', 'samosa', 'sashimi', 'scallops', 'seaweed_salad', 'shrimp_and_grits', 'spaghetti_bolognese', 'spaghetti_carbonara', 'spring_rolls', 'steak', 'strawberry_shortcake', 'sushi', 'tacos', 'takoyaki', 'tiramisu', 'tuna_tartare', 'waffles']
@@ -50,7 +58,9 @@ def preprocess(img_path):
 def predict_image(img_path):
     preprocessed_img = preprocess(img_path)
 
-    raw_outputs = ort_session.run(None, {input_name: preprocessed_img})
+    session, in_name = get_onnx_session()
+
+    raw_outputs = session.run(None, {in_name: preprocessed_img})
     predictions = raw_outputs[0][0]
 
     sorted_indices = np.argsort(predictions)[::-1]
